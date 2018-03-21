4aa75a4c80438821b9e09fab50d4e15e33aa6a03
Hanna Grodzicka <hania.grodzicka@gmail.com>
2018-03-21 22:28:44 +0100

    snakes choose random directions in their boxes

--- src/p1/macos/Snake.cpp
+++ src/p1/macos/Snake.cpp
@@ -9,12 +9,13 @@
 
 using namespace std;
 #define DELAY 100000
+#define SNAKE_LENGHT 10
 
-void make_ball(WINDOW *w, int y, int x, int lenght)
+void make_ball(WINDOW *w, int y, int x)
 {
   //set snake coords
   std::vector<std::pair<int, int>> snake;
-  for (int i = x-lenght; i <= x; ++i)
+  for (int i = x-SNAKE_LENGHT; i <= x; ++i)
   {
     snake.push_back(std::make_pair(y,i));
   }
@@ -23,7 +24,7 @@ void make_ball(WINDOW *w, int y, int x, int lenght)
   int next_x = x;
   int next_y = y;
   int direction_x = 1;
-  int direction_y = 1;
+  int direction_y = 0;
   int change_direction = 0;
 
   getmaxyx(w, max_y, max_x);
@@ -31,49 +32,58 @@ void make_ball(WINDOW *w, int y, int x, int lenght)
   while (1)
   {
     wclear(w);
-    box(w, '|', '-');
+    box(w,0,0);
 
     //paint snake
     for (std::vector<std::pair<int,int>>::iterator it=snake.begin(); it!=snake.end(); ++it)
     {
-      mvwprintw(w, it->first, it->second, "o");
+      mvwprintw(w, it->first, it->second, "O");
     }
 
-    usleep(DELAY);
-
+    //choose random direction after 5 steps
     if (change_direction == 5)
     {
       do
       {
         direction_x = rand() % 3 - 1; 
         direction_y = rand() % 3 - 1;
-      } while (direction_x == 0 && direction_y == 0);
+      } while ((direction_x == 0 && direction_y == 0));
       change_direction = 0;
     }
 
-    //todo
-    snake.erase(snake.begin()); //removes first element (snake's ass)
+    usleep(DELAY);
+
     next_x = x + direction_x;
     next_y = y + direction_y;  
-    snake.push_back(make_pair(next_y, next_x));
 
-    if (next_x >= max_x-1 || next_x < 1)
+    //pls stay in the box
+    //corners
+    if ((next_x >= max_x-2 || next_x < 2) && (next_y >= max_y-2 || next_y < 2))
     {
       direction_x *= -1;
-    }
-    else
-    {
-      x += direction_x;
-    }
-
-    if (next_y >= max_y-1 || next_y < 1)
-    {
       direction_y *= -1;
     }
-    else
+    //borders
+    else 
     {
-      y += direction_y;
+      if (next_x >= max_x-1 || next_x < 1)
+      {
+        direction_x = 0;
+        direction_y = rand() % 2 == 0 ? -1 : 1;
+      }
+      if (next_y >= max_y-1 || next_y < 1)
+      {
+        direction_y = 0;
+        direction_x = rand() % 2 == 0 ? -1 : 1;
+      }
     }
+
+    x += direction_x;
+    y += direction_y;
+
+    //increment snake
+    snake.erase(snake.begin()); //removes first element (snake's ass)
+    snake.push_back(make_pair(y, x));
 
     change_direction++;
   }
@@ -92,54 +102,31 @@ void refresh_windows(WINDOW *a, WINDOW *b, WINDOW *c, WINDOW *d)
 
 int main(int argc, char *argv[])
 {
- /* initialize random seed: */
+ /* initialize random seed */
   srand(time(NULL));
-
-  int ax = 5, ay = 5;
-  int bx = 5, by = 5;
-  int cx = 5, cy = 5;
-  int dx = 5, dy = 5;
-  WINDOW *a, *b, *c, *d;
-  char command[100];
-  int maxx, maxy, halfx, halfy;
   
   initscr();
   noecho();
   curs_set(FALSE);
   refresh();
   
-  start_color();
-  init_pair(1, COLOR_BLACK, COLOR_BLUE);
-  init_pair(2, COLOR_BLACK, COLOR_RED);
-  init_pair(3, COLOR_BLACK, COLOR_GREEN);
-  init_pair(4, COLOR_BLACK, COLOR_CYAN);
   /* calculate window sizes and locations */
+  int maxx, maxy, halfx, halfy;
   getmaxyx(stdscr, maxy, maxx);
   halfx = maxx >> 1;
   halfy = maxy >> 1;
+  
   /* create four windows to fill the screen */
+  WINDOW *a, *b, *c, *d;
   a = newwin(halfy, halfx, 0, 0);
   b = newwin(halfy, halfx, 0, halfx);
   c = newwin(halfy, halfx, halfy, 0);
   d = newwin(halfy, halfx, halfy, halfx);
-  /* Write to each window */
-  box(a, '|', '-');
-  box(b, '|', '-');
-  box(c, '|', '-');
-  box(d, '|', '-');
-  mvwaddstr(a, 0, 0, "");
-  wrefresh(a);
-  mvwaddstr(b, 0, 0, "");
-  wrefresh(b);
-  mvwaddstr(c, 0, 0, "");
-  wrefresh(c);
-  mvwaddstr(d, 0, 0, "");
-  wrefresh(d);
-
-  thread t1(make_ball, a, halfy/2, halfx/2, 5);
-  thread t2(make_ball, b, halfy/2, halfx/2, 5);
-  thread t3(make_ball, c, halfy/2, halfx/2, 5);
-  thread t4(make_ball, d, halfy/2, halfx/2, 5);
+  
+  thread t1(make_ball, a, halfy/2, halfx/2);
+  thread t2(make_ball, b, halfy/2, halfx/2);
+  thread t3(make_ball, c, halfy/2, halfx/2);
+  thread t4(make_ball, d, halfy/2, halfx/2);
   thread t5(refresh_windows, a, b, c, d);
 
   t1.join();
